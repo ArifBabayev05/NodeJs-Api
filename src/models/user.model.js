4697ca2036e272d82de33916c450857cec9df9c5
Arif Babayev <arifrb@code.edu.az>
2023-04-22 17:38:04 +0400

    User Model

---
+++ src/models/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const userScheme  = new mongoose.Schema({
+    name:{type:String,required:true,trim:true},
+    lastName:{type:String,required:true,trim:true},
+    email:{type:String,required:true,trim:true,unique:true},
+    password:{type:String,required:true,trim:true},
+},{collection: "users",timestamps:true})
+
+const user = mongoose.model("users",userSchema);
